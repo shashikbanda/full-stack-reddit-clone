47e9649f75a8df2f88465d5c32630d0aacf97994
Shashik Banda <shashik.banda@gmail.com>
2016-02-17 11:37:54 -0800

    required more node modules. set up all knex related files and database on local machine

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var knex = require('./db/knex');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,5 +13,5 @@ app.get('/', function(req, res){
 })
 
 app.listen('3000', function(){
-	console.log("200")
+
 })
