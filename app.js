db1f6a51aba53bd2ae35cf1b43ad006ae5b061ca
Shashik Banda <shashik.banda@gmail.com>
2016-02-19 09:43:20 -0800

    able to detect angular in index html when served from express server. still error though in detecting MainController

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var ejs = require('ejs')
 
 var posts = require('./routes/posts')
 
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/partials', express.static(__dirname + '/partials'))
+// app.use('/js', express.static(__dirname + '/public'));
+// app.use('/partials', express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', posts)
 
 app.get('*', function(req, res){
-	res.sendFile('index.html', { root: __dirname + '/assets' });
+	res.sendFile(__dirname +'/public/index.html');
 })
 
 app.listen('3000', function(){
