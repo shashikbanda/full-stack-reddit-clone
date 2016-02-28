d7836319c2c79505570d82ec77cf1d5d9877fe68
Shashik Banda <shashik.banda@gmail.com>
2016-02-28 15:36:10 -0800

    created new migrations table with an auto-incrementing column to use as a foreign key for comments table.

--- migrations/20160217144306_appdb.js
+++ migrations/20160217144306_appdb.js
@@ -1,12 +1,14 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
 	knex.schema.createTable("posts", function(table){
+		table.increments('postid').primary();
 	    table.string('title');
 	    table.string('author');
 	    table.string('imageUrl');
 	  	table.string('description');
   	}),
   	knex.schema.createTable("comments", function(table){
+  		table.integer('postid').references('postid').inTable("posts");
 		table.string('comment');
   	})])
 };
